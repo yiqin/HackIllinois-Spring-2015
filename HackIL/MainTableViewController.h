ecf7e6632f8056e638ca425cee9698ff7efe4c17
yiqin <yiqin.mems@gmail.com>
2015-02-28 02:11:57 -0600

    Build MainTableViewController.

---
+++ HackIL/MainTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainTableViewController.h
+//  HackIL
+//
+//  Created by Yi Qin on 2/28/15.
+//  Copyright (c) 2015 Yi Qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTableViewController : UITableViewController
+
+@end
