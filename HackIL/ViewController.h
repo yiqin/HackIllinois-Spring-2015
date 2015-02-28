35c7f0cd76012798e2be1559e7a2035ddf6ab1a8
yiqin <yiqin.mems@gmail.com>
2015-02-27 18:47:51 -0600

    Initial Commit

---
+++ HackIL/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HackIL
+//
+//  Created by Yi Qin on 2/27/15.
+//  Copyright (c) 2015 Yi Qin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HackIL/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HackIL
+//
+//  Created by Yi Qin on 2/27/15.
+//  Copyright (c) 2015 Yi Qin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
