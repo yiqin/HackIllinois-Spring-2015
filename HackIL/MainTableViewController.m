878aaf5aa93d0700edbe25b2a4c41eb005e4c93b
yiqin <yiqin.mems@gmail.com>
2015-03-01 00:44:35 -0600

    hello.....

--- HackIL/MainTableViewController.m
+++ HackIL/MainTableViewController.m
@@ -21,6 +21,7 @@
 
 @property(nonatomic, strong) NSIndexPath *clickingIndexPath;
 
+@property(nonatomic, strong) UIImageView *launchImageView;
 
 
 @end
@@ -33,6 +34,8 @@
         self.objects = [[NSArray alloc] init];
         self.clickedCollection = [[NSMutableSet alloc] init];
         self.clickingIndexPath = [NSIndexPath indexPathForItem:1 inSection:0];
+        
+        self.launchImageView = [[UIImageView alloc] init];
     }
     return self;
 }
@@ -41,7 +44,15 @@
     self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
     
     [super viewDidLoad];
-
+    
+    
+    
+    
+    [[UIApplication sharedApplication].keyWindow addSubview:self.launchImageView];
+    
+    
+    
+    
     [self.navigationController setValue:[[YALNavigationBar alloc]init] forKeyPath:@"navigationBar"];
     
     self.title = @"Discover";
@@ -66,6 +77,11 @@
     self.refreshControl = refreshControl;
     [self.tableView addSubview:refreshControl];
     
+}
+
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    self.launchImageView.frame = CGRectMake(0, 0, CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame));
 }
 
 - (BOOL)prefersStatusBarHidden {
